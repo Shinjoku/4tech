e69d56abee0edd1ba589ded9ff94069bbb84f749
Shinjoku <deora.2@hotmail.com>
2018-07-17 23:20:27 -0300

    Primeiro Desafio concluído

--- js/script.js
+++ js/script.js
@@ -1,38 +1,16 @@
 var jobs = []
 var posts = 0;
 
-function rowBuilder(){
-  if(posts == 0){
-    return {
-      begin: '<div class="row">',
-      end: ''
-    }
-  } else if(posts < 2) {
-    return {
-      begin: '',
-      end: ''
-    }
-  } else{
-    posts = -1;
-    return {
-      begin: '',
-      end: '</div>'
-    }
-  }
-}
+
 
 function showJob(job) {
-  var container = document.getElementById("container");
-  var html;
+  var deck = document.getElementById("deck");
+
 
   if(job != undefined) {
 
-    html = rowBuilder();
+    deck.innerHTML +=
 
-    console.log("Begin: " + html.begin + "\nEnd: " + html.end);
-
-    container.innerHTML +=
-      html.begin +
       '<div class="card col-sm-12 col-md-6 col-lg-4 mb-3"><img class="card-img-top" src="./venturus/developer.png" alt="Card image cap"><div class="card-body"><h5 class="card-title">' +
       job.name +'</h5><p class="card-text"><p>Descrição: '+
       job.description + '</p><p>Habilidades Necessárias: ' +
